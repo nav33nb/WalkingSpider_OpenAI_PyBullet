bfc24566b64a903a8e2be9565e3396868dbd6fc0
Gurcharan Singh <gurcharan0993@gmail.com>
2020-10-15 19:13:19 +0530

    Stop simulation if the bot falls
    - Add new spider env file
    - Implement falling check

--- environment/walking-spider/walking_spider/envs/__init__.py
+++ environment/walking-spider/walking_spider/envs/__init__.py
@@ -1,3 +1,3 @@
-from walking_spider.envs.walking_spider_env import WalkingSpiderEnv
-
+# from walking_spider.envs.walking_spider_env import WalkingSpiderEnv
+from walking_spider.envs.spider_env import WalkingSpiderEnv
 
