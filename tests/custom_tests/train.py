16b44098b7818069bc3f2111d1d2114705796345
Gurcharan Singh <gurcharan0993@gmail.com>
2020-10-15 19:15:04 +0530

    separate model training file

---
+++ tests/custom_tests/train.py
@@ -0,0 +1,17 @@
+import gym
+import walking_spider
+import pybullet as p
+import pybullet_data
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
+
+env = gym.make('WalkingSpider-v0')
+
+# Optional: PPO2 requires a vectorized environment to run
+# the env is now wrapped automatically when passing it to the constructor
+# env = DummyVecEnv([lambda: env])
+
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=10000)
